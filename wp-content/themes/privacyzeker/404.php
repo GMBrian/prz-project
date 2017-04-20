e87aa990caa50f511b80dc9808dacdd25c12865a
GMBrian <brian@geusmedia.nl>
2017-04-21 00:20:17 +0200

    404 update

--- wp-content/themes/privacyzeker/404.php
+++ wp-content/themes/privacyzeker/404.php
@@ -1,7 +1,12 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<?php get_template_part( 'templates/page', 'header' ); ?>
 
-<div class="alert alert-warning">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+			<h1><?php _e( '404 - Niet gevonden', 'sage' ); ?></h1>
+			<p><?php _e( 'Sorry, maar de pagina die je zocht bestaat niet.', 'sage' ); ?></p>
+		</div>
+	</div>
 </div>
 
-<?php get_search_form(); ?>
+<?php //get_search_form(); ?>
